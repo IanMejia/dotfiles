f9d6a7f1bd023fced49e5faeefda9488d0968705
IanMejia <iankilljoy23@gmail.com>
2021-01-21 15:30:52 -0500

    Latest additions

---
+++ config/sxhkd/shot.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+NAME=~/Pictures/$(date +%y-%m-%d-%T)-shot.png
+
+case "$1" in
+    --window)
+        maim -i $(xdotool getactivewindow) $NAME;;
+    --sel)
+        maim -s -u $NAME;;
+    *)
+        maim $NAME;;
+esac
+
+notify-send "Shot saved at ~/Pictures"
