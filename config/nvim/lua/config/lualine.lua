1d0e1df241354cf5536e68c49b6d1aea7c085a0d
Ian Mejia <inmejia23@gmail.com>
2023-09-02 20:31:29 -0500

    Chore: remove old neovim config files

--- config/nvim/lua/config/lualine.lua
+++ config/nvim/lua/config/lualine.lua
@@ -110,7 +110,7 @@ ins_left({
       t = colors.red,
     }
     vim.api.nvim_command('hi! LualineMode guifg=' .. mode_color[vim.fn.mode()] .. ' guibg=' .. colors.bg)
-    return ''
+    return ''
   end,
   color = 'LualineMode',
   padding = { right = 1 },
