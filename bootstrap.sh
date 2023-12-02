8752324dc36baecd46c663f310491c371206dc51
Ian Mejia <inmejia23@gmail.com>
2023-12-02 16:45:42 -0500

    Chore: add macos packages and separate bootstrap linux logic

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,45 +23,38 @@ if [ ! -d "$HOME/.local/share" ]; then
     mkdir $HOME/.local/share
 fi
 
-# config files
-ln -s $BASEDIR/config/nvim/init.lua $HOME/.config/nvim/init.lua
-ln -s $BASEDIR/config/nvim/lua $HOME/.config/nvim/
-ln -s $BASEDIR/config/zathura $HOME/.config/
-ln -s $BASEDIR/config/rofi $HOME/.config
-ln -s $BASEDIR/config/picom $HOME/.config
-ln -s $BASEDIR/config/dunst $HOME/.config
-
+# ---------------------------
+# common
+# ---------------------------
 # zsh
 ln -s $BASEDIR/zsh/zpreztorc $HOME/.zpreztorc
 ln -s $BASEDIR/zsh/zprofile $HOME/.zprofile
 ln -s $BASEDIR/zsh/zlogin $HOME/.zlogin
 ln -s $BASEDIR/zsh/zshrc $HOME/.zshrc
 
-# x-stuff
-ln -s $BASEDIR/Xresources $HOME/.Xresources
-ln -s $BASEDIR/xinit $HOME/.xinitrc
-
-# bspwm
-ln -s $BASEDIR/config/bspwm $HOME/.config/
-ln -s $BASEDIR/config/sxhkd $HOME/.config/
-ln -s $BASEDIR/config/polybar $HOME/.config/
-
-# kitty
+# config files
+ln -s $BASEDIR/config/nvim/init.lua $HOME/.config/nvim/init.lua
+ln -s $BASEDIR/config/nvim/lua $HOME/.config/nvim/
 ln -s $BASEDIR/config/kitty $HOME/.config/
 
 # fonts
 ln -s $BASEDIR/fonts $HOME/.local/share/
 
-# git commands
-echo "Do you want to install neovim packer?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes )
-            echo "Installing Packer.nvim..."
-            git clone https://github.com/wbthomason/packer.nvim\
-             $HOME/.local/share/nvim/site/pack/packer/opt/packer.nvim
-            break;;
-        No  ) break;;
-        *   ) echo "Pease answer yes or no." ;;
-    esac
-done
+# ---------------------------
+# os-related additions
+# ---------------------------
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # config files
+    ln -s $BASEDIR/config/zathura $HOME/.config/
+    ln -s $BASEDIR/config/rofi $HOME/.config
+    ln -s $BASEDIR/config/picom $HOME/.config
+    ln -s $BASEDIR/config/dunst $HOME/.config
+
+    # x-stuff
+    ln -s $BASEDIR/Xresources $HOME/.Xresources
+    ln -s $BASEDIR/xinit $HOME/.xinitrc
+
+    # bspwm
+    ln -s $BASEDIR/config/bspwm $HOME/.config/
+    ln -s $BASEDIR/config/sxhkd $HOME/.config/
+    ln -s $BASEDIR/config/polybar $HOME/.config/
