b92640d998a85d34e4be42e2f1e7c4ac31a7f08c
Ian Mejia <inmejia23@gmail.com>
2024-02-04 15:26:28 -0500

    Chore: update cmp config to avoid selecting first option

--- config/nvim/lua/config/cmp.lua
+++ config/nvim/lua/config/cmp.lua
@@ -130,7 +130,7 @@ cmp.setup {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
   },
   sources = cmp.config.sources({
     { name = 'copilot' },
